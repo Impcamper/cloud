a028d3cb0d9253bd8cc7a7e3644c66dc1545854b
Impcamper <impcamper@gmail.com>
2023-10-14 21:39:53 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Overview
+
+{Important! Do not say in this section that this is college assignment. Talk about what you are trying to accomplish as a software engineer to further your learning.}
+
+{Provide a description of the software that you wrote and how it integrates with a Cloud Database. Describe how to use your program.}
+
+{Describe your purpose for writing this software.}
+
+{Provide a link to your YouTube demonstration. It should be a 4-5 minute demo of the software running, a walkthrough of the code, and a view of the cloud database.}
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Cloud Database
+
+I am using the firestore database
+
+It has a collection of users, which each contain a collection of the days marked
+
+# Development Environment
+
+I used Visual Studio code, and Python.
+
+# Useful Websites
+
+No. Nothing was usefull. Not even the offical documentation helped. 100% painfull trial and error.
+
+# Future Work
+
+
+- Add password system
+- Find cleaner way to read data
---
+++ main.py
@@ -0,0 +1,41 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account.
+cred = credentials.Certificate('C:\\Users\\impca\\Documents\\fall projects\\key.json') #REMOVE BEFORE END
+
+app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+
+#get user or new user
+print("Welcome to the tracker!")
+userdata=input("Are you an existing user or new user?\nNew for new user, Username for existing user.")
+
+if(userdata.lower()=="new"):
+    #user setup
+    username=input("Enter your username")
+    ref=db.collection("users").document(username.lower())
+    task=input("Enter the daily task")
+    ref.set({"task":task})
+    ref.collection("checklist")#I don't think this is needed
+else:
+    ref=db.collection("users").document(userdata.lower())#simple get existing user
+
+#menue
+menue=-1
+while(menue!=0):
+    print("1 to register day\n2 for info\n0 to quit.")
+    menue=int(input())
+    if(menue==0):#quiting
+        break
+    elif(menue==1):#entering a day into the list
+        date=input("enter the day mm/dd/yyyy\n")
+        ref.collection("checklist").document("Daily").set({date:"done"})
+        print("-----")
+    elif(menue==2):#print the list
+        for doc in ref.collection("checklist").stream():
+            print(doc.to_dict())
+            print("-----")
